15310232a23b2671e719dd36d7b0d57a06922a3c
limonikas@me.com <limonikas@me.com>
2021-09-30 02:28:55 +0300

    Input with formik

--- lib/components/Input/Input.tsx
+++ lib/components/Input/Input.tsx
@@ -1,26 +1,32 @@
-import { FC, InputHTMLAttributes } from 'react';
+import { FC } from 'react';
+import { useField, FieldHookConfig } from 'formik';
 
-export interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+export type InputProps = FieldHookConfig<string> & {
   label?: string;
-  name?: string;
-}
+};
 
-export const Input: FC<InputProps> = ({ label, name, ...props }) => {
+export const Input: FC<InputProps> = (props) => {
+  const [field, meta, helpers] = useField(props);
+  const { label, type, placeholder } = props;
+
   return (
     <div>
       {label && (
-        <label
-          htmlFor={name}
-          className="block text-sm text-gray-800 dark:text-gray-200"
-        >
+        <label className="block text-sm text-gray-800 dark:text-gray-200">
           {label}
         </label>
       )}
+
       <input
-        id={name}
-        {...props}
+        {...field}
+        type={type}
+        placeholder={placeholder}
         className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring"
       />
+
+      {meta.touched && meta.error ? (
+        <p className="text-red-600">{meta.error}</p>
+      ) : null}
     </div>
   );
 };
