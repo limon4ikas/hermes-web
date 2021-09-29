cc756082288f6a7bfce60aa5f3f5d4d8f896e421
limonikas@me.com <limonikas@me.com>
2021-09-30 02:29:03 +0300

    Button with variants

--- lib/components/Button/Button.tsx
+++ lib/components/Button/Button.tsx
@@ -1,14 +1,30 @@
-import { FC, HTMLAttributes } from 'react';
+import { FC, ButtonHTMLAttributes, DetailedHTMLProps } from 'react';
 
-export interface ButtonProps extends HTMLAttributes<HTMLButtonElement> {}
+export const buttonVariants = {
+  primary:
+    'px-4 py-2 leading-5 text-white w-full transition-colors duration-200 transform bg-gray-700 rounded hover:bg-gray-600 focus:outline-none',
+  google:
+    'flex items-center justify-center w-full px-6 py-2 mx-2 text-sm font-medium text-white transition-colors duration-200 transform bg-blue-500 rounded-md hover:bg-blue-400 focus:bg-blue-400 focus:outline-none',
+};
 
-export const Button: FC<ButtonProps> = ({ children, ...props }) => {
+export type ButtonProps = DetailedHTMLProps<
+  ButtonHTMLAttributes<HTMLButtonElement>,
+  HTMLButtonElement
+> & {
+  isLoading?: boolean;
+  variant?: keyof typeof buttonVariants;
+};
+
+export const Button: FC<ButtonProps> = ({
+  variant = 'primary',
+  isLoading,
+  children,
+  className = '',
+  ...props
+}) => {
   return (
-    <button
-      {...props}
-      className="w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-gray-700 rounded-md hover:bg-gray-600 focus:outline-none focus:bg-gray-600"
-    >
-      {children}
+    <button {...props} className={`${buttonVariants[variant]} ${className}`}>
+      {isLoading ? <span>Loading...</span> : children}
     </button>
   );
 };
