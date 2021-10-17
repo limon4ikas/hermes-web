be16cc8243d44dfc9df9d833a4551950a56d4e9b
limonikas@me.com <limonikas@me.com>
2021-10-17 11:18:32 +0300

    Delete className prop from text field

--- lib/components/TextField/component.tsx
+++ lib/components/TextField/component.tsx
@@ -11,8 +11,8 @@ export type TextFieldProps = ComponentPropsWithRef<'div'> &
 
 export const TextField = forwardRef<HTMLDivElement, TextFieldProps>(
   (props, ref) => {
-    const [field, meta, helpers] = useField(props);
     const { label, type, placeholder } = props;
+    const [field, meta, helpers] = useField(props);
 
     return (
       <div ref={ref}>
@@ -22,7 +22,6 @@ export const TextField = forwardRef<HTMLDivElement, TextFieldProps>(
           id={field.name}
           type={type}
           placeholder={placeholder}
-          className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring"
         />
         {meta.touched && meta.error && (
           <TextFieldError>{meta.error}</TextFieldError>
