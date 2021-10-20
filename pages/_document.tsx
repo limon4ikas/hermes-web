4789de970d81cc10b12c23b52eaa4456981d584a
limonikas@me.com <limonikas@me.com>
2021-10-20 12:28:35 +0300

    Remove emotion

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,4 +1,3 @@
-import { Fragment } from 'react';
 import Document, {
   Html,
   Head,
@@ -6,23 +5,10 @@ import Document, {
   NextScript,
   DocumentContext,
 } from 'next/document';
-import { extractCritical } from '@emotion/server';
 
 class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
     const initialProps = await Document.getInitialProps(ctx);
-
-    const critical = extractCritical(initialProps.html);
-    initialProps.html = critical.html;
-    initialProps.styles = (
-      <Fragment>
-        {initialProps.styles}
-        <style
-          data-emotion-css={critical.ids.join(' ')}
-          dangerouslySetInnerHTML={{ __html: critical.css }}
-        />
-      </Fragment>
-    );
 
     return { ...initialProps };
   }
